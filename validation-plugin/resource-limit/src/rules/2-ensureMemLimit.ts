e6e7788ea4365ace0e4e9afc4dd7d44af09b67b2
Sonali Srivastava <srivastava.sonali1@gamil.com>
2023-08-21 18:01:49 +0530

    removed const var

--- validation-plugin/resource-limit/src/rules/2-ensureMemLimit.ts
+++ validation-plugin/resource-limit/src/rules/2-ensureMemLimit.ts
@@ -6,14 +6,13 @@ import { isDeployment } from "../schemas/__generated__/deployment.apps.v1.js";
 export const ensureMemLimit = defineRule({
   id: 2,
   description: "Please ensure mem limit is set to 1G for dev env deployments",
-  help: "Values allowed less than 1G",
+  help: "Value allowed is 1G",
   validate({ resources }, { report }) {
     resources.filter(isDeployment).forEach((deployment) => {
       const containers = deployment.spec?.template?.spec?.containers;
       if (containers) {
         for (const container of containers) {
-          const resources = container.resources;
-            if( resources?.limits?.memory !== '1G' ){
+            if( container.resources?.limits?.memory !== '1G' ){
               report(deployment, { path: "spec.template.spec.containers" });
            }
         }
